8a879bfd116554d7ecb652d41d751ee34c68f5b1
Hugo Mercado <hugo.mario.mercado@gmail.com>
2025-02-27 18:49:36 -0500

    pangaea project following book ue c++ till chapter 6

---
+++ Source/Pangaea/PlayerAvatar.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerAvatar.h"
+
+#include "PlayerAvatarAnimInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values
+APlayerAvatar::APlayerAvatar()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Create the camera spring arm
+	_springArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	_springArmComponent->SetupAttachment(RootComponent);
+	_springArmComponent->SetUsingAbsoluteRotation(true);
+	_springArmComponent->TargetArmLength = 800.f;
+	_springArmComponent->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	_springArmComponent->bDoCollisionTest = false;
+
+	//Create the camera
+	_cameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	_cameraComponent->SetupAttachment(_springArmComponent, USpringArmComponent::SocketName);
+	_cameraComponent->bUsePawnControlRotation = false;
+
+	// flag to indicate tick method will be called with the game's frame update
+	PrimaryActorTick.bCanEverTick = true;
+
+	// avoid player to control rotation:
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	auto characterMovement = GetCharacterMovement();
+	characterMovement->bOrientRotationToMovement = true;
+	characterMovement->RotationRate = FRotator(0.f, 640.f, 0.f);
+	characterMovement->bConstrainToPlane = true;
+	characterMovement->bSnapToPlaneAtStart = true;
+}
+
+// Called when the game starts or when spawned
+void APlayerAvatar::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void APlayerAvatar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UPlayerAvatarAnimInstance* animInstance = Cast<UPlayerAvatarAnimInstance>(GetMesh()->GetAnimInstance());
+	animInstance->Speed = GetCharacterMovement()->Velocity.Size2D();
+}
+
+// Called to bind functionality to input
+void APlayerAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+int APlayerAvatar::GetHealthPoints()
+{
+	return _HealthPoints;
+}
---
+++ Source/Pangaea/PlayerAvatar.h
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "PlayerAvatar.generated.h"
+
+UCLASS(Blueprintable)
+class PANGAEA_API APlayerAvatar : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerAvatar();
+
+	UPROPERTY(EditAnywhere, Category="Player Params")
+	int HealthPoints = 500;
+
+	UPROPERTY(EditAnywhere, Category="Player Params")
+	float Strength = 10;
+
+	UPROPERTY(EditAnywhere, Category="Player Params")
+	float Armer = 3;
+
+	UPROPERTY(EditAnywhere, Category="Player Params")
+	float AttackRange = 6.0f;
+
+	UPROPERTY(EditAnywhere, Category="Player Params")
+	float AttackInterval = 1.2f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	int _HealthPoints;
+	float _AttackCountingDown;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	int GetHealthPoints();
+	bool IsKilled();
+	bool CanAttack();
+	void Attack();
+	void Hit(int damage);
+
+	FORCEINLINE USpringArmComponent* GetSringArmComponent() const
+	{
+		return _springArmComponent;
+	}
+	FORCEINLINE UCameraComponent* GetCameraComponent() const
+	{
+		return _cameraComponent;
+	}
+
+protected:
+	void DieProcess();
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* _springArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* _cameraComponent;
+};
